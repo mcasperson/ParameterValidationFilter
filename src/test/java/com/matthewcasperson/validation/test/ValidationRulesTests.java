7398ea4dbb9265eb7280ba0af18ea3628e953d6a
Matthew Casperson <matthewcasperson@gmail.com>
2014-12-18 08:22:47 +1000

    Added another test

--- src/test/java/com/matthewcasperson/validation/test/ValidationRulesTests.java
+++ src/test/java/com/matthewcasperson/validation/test/ValidationRulesTests.java
@@ -92,6 +92,23 @@ public class ValidationRulesTests {
 			Assert.fail();
 		}
 	}
+
+	@Test
+	public void testEncode5() {
+		try {
+			//final String testString = "Please Choose…";
+			final String testString = "Please Choose&hellip;";
+			final HTMLEncodeTextValidationRule rule1 = new HTMLEncodeTextValidationRule();
+			final CanonicalizeTextValidationRule rule2 = new CanonicalizeTextValidationRule();
+			Assert.assertEquals(
+					testString,
+					rule1.fixParam("test", "test", rule2.fixParam("test", "test", testString)));
+		} catch (final ValidationFailedException ex) {
+			Assert.fail();
+		}
+	}
+
+
 	
 	@Test
 	public void testRegex1() {
